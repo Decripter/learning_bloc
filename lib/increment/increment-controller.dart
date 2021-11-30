2759f6b39e69b5daaa3e12c3db1aacd08c06057e
Decripter <jeilsonaraujo@icloud.com>
2021-11-30 16:19:48 -0300

    conf: adicionando uma classe increment controller para implementar o padrao bloc, ou seja, separar a interface da regra de negocio, para isso temos a classe increment controller que extend de bloc_pattern

---
+++ lib/increment/increment-controller.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:bloc_pattern/bloc_pattern.dart';
+
+class IncrementController implements BlocBase {
+  @override
+  void addListener(VoidCallback listener) {
+    // TODO: implement addListener
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  // TODO: implement hasListeners
+  bool get hasListeners => throw UnimplementedError();
+
+  @override
+  void notifyListeners() {
+    // TODO: implement notifyListeners
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    // TODO: implement removeListener
+  }
+}
