677df99aa32c93866745e0f5be7502024c225628
Decripter <jeilsonaraujo@icloud.com>
2021-11-30 20:22:35 -0300

    conf: adicionando o Stream para prover o canal de transferencia dos dados entre o controller e a view

--- lib/increment/increment-controller.dart
+++ lib/increment/increment-controller.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:bloc_pattern/bloc_pattern.dart';
 
 class IncrementController implements BlocBase {
+  var counter = 0;
+  var _counterController = StreamController<int>();
+
   @override
   void addListener(VoidCallback listener) {
     // TODO: implement addListener
